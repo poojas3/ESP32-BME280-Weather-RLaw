3524919d53dc4998f6e94fa91fd264b67f41008e
Ganesh Surana <gsurana@tibco.com>
2020-07-22 16:30:38 -0600

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -31,43 +31,6 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
-  refresh_readings();
-  delay(4000);
-}
-
-void refresh_readings() {
-  float f_temperature;
-  float f_humidity;
-  float f_pressure;
-  float f_altitude;
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  f_temperature = bme.readTemperature();
-  f_humidity = bme.readHumidity();
-  f_pressure = bme.readPressure() / 100.0F;
-  f_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-  // Temperature
-  Serial.print(f_temperature);
-  Serial.println(" °C");
-
-  // Humidity
-  Serial.print(f_humidity);
-  Serial.println(" %");
-
-  // Pressure
-  Serial.print(f_pressure);
-  Serial.println(" hPa");
-
-  // Appx altitude
-  Serial.print(f_altitude);
-  Serial.println(" m");   
-  
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("-----v1-----");   
-=======
   refresh_readings(bme);  // Passing the bme object to the function as bme is only "global" in this file.
   delay(2000);
->>>>>>> 68bdcb933f3d7bf8d3e3b748f0c79047a29b1d52
 }
